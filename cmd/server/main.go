87bf0c54b6420beed298765dbcef05df05dd1d4d
alilou <aliopdandan@gmail.com>
2026-03-30 09:46:55 +0100

    f

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,11 +10,11 @@ import (
 func handler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(service.CreateResume())
-	fmt.Fprintln(w, "Hello world")
 }
 
 func main(){
 	http.HandleFunc("/create-resume", handler)
 
+	fmt.Println("Server is running...")
 	http.ListenAndServe(":8000", nil)
 }
